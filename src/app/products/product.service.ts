c4b4a2ebf0e63aea295d0f51bf6c46e012ce62ab
Aravind Kumar J <aravind2808@gmail.com>
2020-12-21 18:42:20 +0530

    Angular Routing Implementation

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { IProduct } from './product';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http'
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/Operators';
+import { catchError, tap, map } from 'rxjs/Operators';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +17,13 @@ export class ProductService {
   getProducts(): Observable<IProduct[]> {
     return this.http.get<IProduct[]>(this.productUrl).pipe(
       tap(data => console.log('All: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  getProduct(id: number): Observable<IProduct | undefined> {
+    return this.getProducts().pipe(
+      map((products: IProduct[]) => products.find(p => p.productId === id)),
       catchError(this.handleError)
     );
   }
