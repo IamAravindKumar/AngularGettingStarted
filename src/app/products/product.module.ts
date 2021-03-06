68cdd076c9a8c81467d9143d88a1dc7d9cf2e353
Aravind Kumar J <aravind2808@gmail.com>
2021-03-06 12:30:07 +0530

    Shared module

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductListComponent } from './product-list.component';
-import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
 import { SharedModule } from '../shared/shared.module';
 import { ProductRoutingModule } from './product-routing.module';
 
@@ -9,7 +8,6 @@ import { ProductRoutingModule } from './product-routing.module';
 @NgModule({
   declarations: [    
     ProductListComponent,
-    ConvertToSpacesPipe,
     ProductDetailComponent
   ],
   imports: [
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { StarComponent } from '../shared/star.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
 
 
 
 @NgModule({
   declarations: [
-    StarComponent
+    StarComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     CommonModule
   ],
   exports: [
     StarComponent,
+    ConvertToSpacesPipe,
     CommonModule,
     FormsModule
   ]
